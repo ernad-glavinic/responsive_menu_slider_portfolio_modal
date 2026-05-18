afe29e484169a44eec5e02639dd5373a9a6198a1
Korisnik <ernadglavinic@gmail.com>
2026-05-18 22:10:13 +0200

    Added JavaScript Logic for slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This is code for RESPONSIVE MENU
 window.addEventListener('resize', e =>{
     let menu = document.querySelector('.header ul');
     let btn = document.querySelector('.header button');
@@ -21,5 +22,32 @@ const mobileMenu = () => {
     }   
 }
 
+// This is code for SLIDER
+let rightBtn = document.querySelector('#right-btn');
+let leftBtn = document.querySelector('#left-btn');
+let pictures = document.querySelectorAll('.slider-images img');
+let imageNum = 0;
 
+const moveRight = () => {
+    displayNone(pictures);
+    imageNum++;
+    
+    if(imageNum === pictures.length) imageNum = 0;
+  
+    pictures[imageNum].style.display = 'block';
+}
 
+const moveLeft = () => {
+    displayNone(pictures);
+    imageNum--;
+    
+    if(imageNum < 0) imageNum = pictures.length - 1;
+  
+    pictures[imageNum].style.display = 'block';
+}
+
+rightBtn.addEventListener('click', moveRight);
+leftBtn.addEventListener('click', moveLeft);
+
+// Function will hide all images
+const displayNone = elements => elements.forEach(img => img.style.display = 'none');
