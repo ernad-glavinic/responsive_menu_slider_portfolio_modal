cd66aeaec13887b980c02a602ae0a414a0faf811
Korisnik <ernadglavinic@gmail.com>
2026-05-18 21:31:14 +0200

    Added JavaScript logic for responsive menu. It has bug. When resize to smaller device (<1000px) everything is working. When resize back to wider screen (>1000px) menu is not displayed

---
+++ script.js
@@ -0,0 +1,13 @@
+const mobileMenu = () => {
+    let menu = document.querySelector('.header ul');
+    let btn = document.querySelector('.header button');
+
+    if(btn.innerText === 'MENU'){
+        menu.style.display = 'block';
+        btn.innerText = 'CLOSE';
+    }else{
+        menu.style.display = 'none';
+        btn.innerText = 'MENU';
+    }   
+}
+
